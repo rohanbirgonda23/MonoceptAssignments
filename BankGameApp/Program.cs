2b62d70c0d8ced236b54cb73e04bded11d5f307d
rohanbirgonda23 <rohanbirgonda23@gmail.com>
2026-04-06 18:05:20 +0530

    Organizing File STructure Correctly

---
+++ BankGameApp/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        // Create accounts with random starting balance
+        BankAccount acc1 = new BankAccount("Alice", rand.Next(500, 1001));
+        BankAccount acc2 = new BankAccount("Bob", rand.Next(500, 1001));
+
+        Console.WriteLine("Starting Balances:");
+        acc1.DisplayBalance();
+        acc2.DisplayBalance();
+        Console.WriteLine();
+
+        // 3 rounds
+        for (int round = 1; round <= 3; round++)
+        {
+            Console.WriteLine("---- Round " + round + " ----");
+
+            // Account 1 turn
+            Console.WriteLine("\n" + acc1.AccountHolder + "'s Turn");
+            Console.Write("Enter amount to deposit: ");
+            double dep1 = Convert.ToDouble(Console.ReadLine());
+            acc1.Deposit(dep1);
+
+            Console.Write("Enter amount to withdraw: ");
+            double wit1 = Convert.ToDouble(Console.ReadLine());
+            acc1.Withdraw(wit1);
+
+            acc1.DisplayBalance();
+
+            // Account 2 turn
+            Console.WriteLine("\n" + acc2.AccountHolder + "'s Turn");
+            Console.Write("Enter amount to deposit: ");
+            double dep2 = Convert.ToDouble(Console.ReadLine());
+            acc2.Deposit(dep2);
+
+            Console.Write("Enter amount to withdraw: ");
+            double wit2 = Convert.ToDouble(Console.ReadLine());
+            acc2.Withdraw(wit2);
+
+            acc2.DisplayBalance();
+
+            Console.WriteLine();
+        }
+
+        // Determine Winner
+        Console.WriteLine("---- Final Result ----");
+
+        if (acc1.Balance > acc2.Balance)
+        {
+            Console.WriteLine(acc1.AccountHolder + " wins with balance " + acc1.Balance);
+        }
+        else if (acc2.Balance > acc1.Balance)
+        {
+            Console.WriteLine(acc2.AccountHolder + " wins with balance " + acc2.Balance);
+        }
+        else
+        {
+            Console.WriteLine("It's a tie!");
+        }
+    }
+}
